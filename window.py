5e8f3302f9bf4c3bcaa48428349254e92eaaea1e
Konstantin <zakharov83@mail.ru>
2024-12-15 18:42:51 +0300

    added first gui file to repo

---
+++ window.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+my_window = Tk()
+
+my_window.geometry("600x600")
+my_window.title("My First Python GUI Project")
+
+# icon = PhotoImage(file='photo.jpg')
+# my_window.iconphoto(True, icon) replaces the fabicon
+
+my_window.config(background="black")
+
+my_window.mainloop()
