af66ac2c2330875864023db9ad9f8510b51e7cc3
Konstantin <zakharov83@mail.ru>
2024-12-16 20:41:54 +0300

    added some code to file

--- labels.py
+++ labels.py
@@ -1,7 +1,13 @@
 from tkinter import *
 
 window = Tk()
-window.geometry("300x400")
+window.geometry("600x800")
 window.title('Labels')
+
+
+label = Label(window, text="Hello World!",
+                       font=("Verdana", 60, "bold"),
+                       fg="white", bg="black")
+label.pack()
 window.mainloop() 
 
