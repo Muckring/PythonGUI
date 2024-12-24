5186a1df3f8bbeaf8bb74fa37fee0857055231a2
Konstantin <zakharov83@mail.ru>
2024-12-24 19:32:32 +0300

    added some comments

--- inputs.py
+++ inputs.py
@@ -1,16 +1,23 @@
 from tkinter import *
 
-def submit() -> None:
+# CREATE A FUNCTION FOR THE SUBMIT BUTTON
+
+def submit() -> None: 
     username = entry.get()
     print(username)
+
+# CREATE ROOT
 
 root = Tk()
 root.geometry("1400x1200")
 root.title("User Inputs")
 
+#CREATE SUBMIT BUTTON
+
 submit = Button(root, text="Submit", command=submit)
 submit.pack()
 
+# CREATE USER INPUT ENTRY(FIELD)
 entry = Entry()
 entry.config(font=("TImes New Roman", 50, 'bold'))
 entry.config(bg="black")
@@ -21,6 +28,9 @@ entry.config(fg='#00FF00')
 # entry.config(show="*") show replaces text with specified symbol
 
 entry.pack()
+
+# CONVERT GIF FILE TO PHOTOIMAGE
+# CHANGE ROOT ICON
 
 picture = PhotoImage(file="bonfire.gif")
 root.iconphoto(True, picture)
